43c1f1d243c57b27215b5ef4b08866c559ac5eba
Dhanajit Kapali <dhanajit.kapali@unthinkable.co>
2022-08-23 18:24:37 +0530

    added a tableView

--- DuneBooks/BookList/Presentation/View/BookListViewController.swift
+++ DuneBooks/BookList/Presentation/View/BookListViewController.swift
@@ -8,16 +8,33 @@
 import UIKit
 
 class BookListViewController: UIViewController {
-
+    
+    var bookListSize : Int = 10
     private var viewModel : BookListViewModel!
+    @IBOutlet weak var bookListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         viewModel = BookListViewModel()
+        bookListTableView.dataSource = self
     }
 
 
 }
 
+
+extension BookListViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bookListSize
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = bookListTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "LoL"
+        return cell
+    }
+    
+    
+}
